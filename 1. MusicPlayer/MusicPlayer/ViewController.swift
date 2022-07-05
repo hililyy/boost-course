93a87ae9c92a765c0f526063cd51ad1380f002a9
hililyy <joun406@gmail.com>
2022-07-06 00:06:11 +0900

    commit

---
+++ 1. MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 강조은 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
